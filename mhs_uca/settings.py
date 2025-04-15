e61583811a61259679fb9908722e5610defaea3a
Aisha Toichieva <aisha.toichieva_2025@ucentralasia.org>
2025-04-15 14:03:12 +0600

    fix

--- mhs_uca/settings.py
+++ mhs_uca/settings.py
@@ -224,7 +224,7 @@ if not DEBUG:
     CSRF_TRUSTED_ORIGINS = [
         "https://ucamhs.org",
         "https://www.ucamhs.org",  # Add this too if you support www
-        'https://railway.com',
+        'https://*.railway.app',
     ]
     CSRF_COOKIE_DOMAIN = ".ucamhs.org"
 
